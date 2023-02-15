945f81eea90c4765759ecdcb619102e1181c6629
Chebbi_Mariem <chebbimariem289@gmail.com>
2023-02-15 15:01:49 +0100

    base de données

--- src/Entity/SuperAdministrateur.php
+++ src/Entity/SuperAdministrateur.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SuperAdministrateurRepository::class)]
-class SuperAdministrateur
+class SuperAdministrateur extends Utilisateur
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
